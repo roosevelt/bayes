34a7fb68ca78e3d24f6b24cb32be96c182384edb
Roosevelt <roosevelt1@gmail.com>
2017-11-25 18:38:39 -0200

    just added node and cpd basic construction functionality

--- CPD.py
+++ CPD.py
@@ -1,23 +1,22 @@
 # A conditional probability distribution table to be associated to a node
-import decimal
+from decimal import Decimal
 import copy
 
 class CPD():
     # variables is a list of variables of type Variable
     # parameters is a list of 
-    def __init__(self, variables):
-        variables.sort(key = lambda x: x.name)
-        self.variables = variables
-        self.table = self.__buildTable(self.variables) 
-    
-    def setParameters(parameters):
-        pass
-    
-    def __buildTable(self, variables):
+    def __init__(self, variable, condition_variables=[]):
+        if len(condition_variables)>0:
+            condition_variables.sort(key = lambda x: x.name)
+        self.variable = variable
+        self.condition_variables = condition_variables
+        self.table = self.__buildTable(self.variable, self.condition_variables) 
+        
+    def __buildTable(self, variable, condition_variables):
         table = {}
-        for combination in self.__getVariableValuesCombinations(variables):
+        for combination in self.__getVariableValuesCombinations([variable]+condition_variables):
             f_comb = frozenset(combination)
-            table[f_comb] = decimal.Decimal('0.0')
+            table[f_comb] = Decimal('0.0')
         return table
     
     def __getVariableValuesCombinations(self, variables):
@@ -54,17 +53,44 @@ class CPD():
         
     def __str__(self):
         # Prints this CPD to the stdout
+        n_line = 20
         result = ""
+        result += ("="*n_line) + " Conditional Probability Table of " + self.variable.name  + " (" + str(len(self.variable.values))+ " Values) " + "="*n_line + "\n"
+        condition_arr = []
+        for condition_var in self.condition_variables:
+            condition_arr.append(condition_var.name + " ("  + str(len(condition_var.values)) +  " Values)")        
+        result += "Possible Values: " + ", ".join(self.variable.values) + "\n"
+        if len(self.condition_variables)>0:
+            result +=  "Parents: " + ", ".join(condition_arr) + "\n"
+        else:
+            result += "Parents: None (no parents)\n"
+        result += "Lines: " +  str(len(self.table)) +" entries\n\n"
+    
         for example in self.table:
            str_example = ""
            feature_val_lst = []
-
+           main_feature = ""
            for feature_val in example:
-               feature_val_lst.append(feature_val[0] + "(" + str(feature_val[1]) + ")")
-               feature_val_lst.sort()
+               if(feature_val[0]!=self.variable.name):
+                   feature_val_lst.append(feature_val[0] + "(" + str(feature_val[1]) + ")")
+                   feature_val_lst.sort()
+               else:
+                   main_feature = feature_val[0] + "(" + str(feature_val[1]) + ")"
+           feature_val_lst = [main_feature] + feature_val_lst
 
            for feat_str in feature_val_lst:
                str_example += '{0:30s} '.format(feat_str)
            str_example += '{0:25s} '.format(str(self.table[example]))
            result+=str_example+"\n"
         return result
+    
+    # Set the parameters in the CPD
+    def setParameters(self, parameters):
+        # parameters is a list of parameters in the order that it appears on the table
+        k=0
+        for combination in self.table:
+            self.table[combination]=Decimal(str(parameters[k]))
+            k+=1
+        
+    def getParameters(self):
+        return self.table
--- Node.py
+++ Node.py
@@ -1,10 +1,13 @@
 # A Bayesian Node, with its properties and associated CPD
 class Node():
     
-    def __init__(self, name, CPD):
-        self.name = name
+    def __init__(self, CPD):
+        self.name = CPD.variable.name
         self.CPD = CPD
-        self.parents = []
+        self.parents = CPD.condition_variables
+        
+    def __str__(self):
+        return self.CPD.__str__()
         
     
         
