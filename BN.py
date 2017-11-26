986dd434e75b940d700ef925f7d7ae20e33185f5
Roosevelt <roosevelt1@gmail.com>
2017-11-25 23:04:34 -0200

    now it is possible to export an image of the BN graph

--- BN.py
+++ BN.py
@@ -1,6 +1,7 @@
 # Bayesian network class 
 from Node import Node
 from Edge import Edge
+import pydot
 
 class BayesianNetwork:
     
@@ -28,6 +29,20 @@ class BayesianNetwork:
             buffer+=node.__str__()+ "\n"
         return buffer
             
+    def exportGraph(self, path="bayesian_network.png"):
+        # Generate a picture with a visualization of the network
+        # path is the location where to export the picture
+        graph = pydot.Dot(graph_type='digraph')
+
+        bn_nodes = self.nodes
+        for bn_node in bn_nodes:
+            variable = bn_node.name
+            node =  pydot.Node(variable, style="filled", fillcolor="white")
+            graph.add_node(node)
+            for parent in bn_node.parents:
+                graph.add_edge(pydot.Edge(parent.name, node))
+
+        graph.write_png(path)
                 
                 
     
